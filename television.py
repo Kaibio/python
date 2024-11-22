40ab44de9b9e9f36d8e7d0b95cda1e6d7e7012c2
Anketlb <jetmanplaysminecraft@gmail.com>
2024-11-22 17:48:49 -0600

    Volume bandaid

--- television.py
+++ television.py
@@ -26,19 +26,20 @@ class Television:
             set_status(False)
             get_status()
 
+    def set_mute(self, sound):
+        self.__muted = sound
     def mute(self):
-        def get_mute():
-            return self.__muted
-
         def set_mute(sound):
             self.__muted = sound
-
-        if not get_mute():
-            set_mute(True)
-            get_mute()
-        else:
-            set_mute(False)
-            get_mute()
+        if self.__status:
+            if not self.__muted:
+                set_mute(True)
+                self.set_volume(Television.MIN_VOLUME)
+                return self.__muted
+            else:
+                set_mute(False)
+                self.set_volume(self.__volume + 1)
+                return self.__muted
 
     def channel_up(self):
         def set_channel(num):
@@ -58,6 +59,10 @@ class Television:
                 set_channel(self.__channel - 1)
                 if self.__channel < Television.MIN_CHANNEL:
                     set_channel(Television.MAX_CHANNEL)
+
+    def set_volume(self, num):
+        self.__volume = num
+
     def volume_up(self):
         def set_volume(num):
             self.__volume = num
@@ -67,6 +72,9 @@ class Television:
                 set_volume(self.__volume + 1)
                 if self.__volume > Television.MAX_VOLUME:
                     set_volume(Television.MAX_VOLUME)
+            if self.__muted:
+                self.set_mute(False)
+                set_volume(self.__volume + 1)
     def volume_down(self):
         def set_volume(num):
             self.__volume = num
@@ -76,6 +84,9 @@ class Television:
                 set_volume(self.__volume - 1)
                 if self.__volume < Television.MIN_VOLUME:
                     set_volume(Television.MIN_VOLUME)
+            if self.__muted:
+                self.set_mute(False)
+                set_volume(self.__volume + 1)
 
     def __str__(self):
         return f'power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}'
