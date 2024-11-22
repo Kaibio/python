4376f2a06a1170d7f563401da00a4fbba2526109
Anketlb <jetmanplaysminecraft@gmail.com>
2024-11-22 14:38:38 -0600

    Update Television.py

--- Television.py
+++ Television.py
@@ -1,1 +1,30 @@
+#This is a python file
+#This python file will be called television.py
 
+class Television:
+    MIN_VOLUME = 0
+    MAX_VOLUME = 2
+    MIN_CHANNEL = 0
+    MAX_CHANNEL = 3
+
+    def __init__(self):
+       self.__status = False
+       self.__muted = False
+       self.__volume = Television.MIN_VOLUME
+       self.__channel = Television.MIN_CHANNEL
+
+    def power(self):
+        
+        pass
+    def mute(self):
+        pass
+    def channel_up(self):
+        pass
+    def channel_down(self):
+        pass
+    def volume_up(self):
+        pass
+    def volume_down(self):
+        pass
+    def __str__(self):
+        pass
